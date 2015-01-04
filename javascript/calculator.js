23ee9b37cfb9ad83a611e80ddfbf9f63f47f253f
Jacinda Zhong <jacindaz@gmail.com>
2015-01-04 12:35:00 -0500

    Working version for double digits
     - adds function to convert expression list to a string (so
       that it can be displayed in the window)
     - does not work with decimals yet

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -1,14 +1,15 @@
+// represents most recent # clicked
 var selected_number;
+
+// represents entire # (several digits possible)
 var result = 'Calculate something!';
+
+// represents entire expression to evaluate
 var expression = [];
 
 
 function displayResult(result) {
     $("#calculator-results").replaceWith('<p id="calculator-results">' + result + '</p>');
-}
-
-function calculateSomething() {
-
 }
 
 $("#clear-button").on('click', function() {
@@ -18,23 +19,37 @@ $("#clear-button").on('click', function() {
 })
 
 
-
 $(document).ready(function() {
     $("#1,#2,#3,#4,#5,#6,#7,#8,#9,#0").on("click", function() {
         var selected_element = $(this);
         selected_number = selected_element.text();
-        if (result === 'Calculate something!') {
+        if (result === 'Calculate something!' || result == '') {
           result = selected_number;
+          expression.push(result);
+        } else if (result != '') {
+          result += selected_number;
+          expression[expression.length - 1] = result;
         }
-        expression.push(selected_number);
-        displayResult(result);
+        console.log('Expression: ' + expression);
+        console.log('Result: ' + result);
+
+        var expression_string = array_to_string(expression);
+        displayResult(expression_string);
     });
 })
 
 $("#plus,#minus,#divide,#multiply").on("click", function() {
+    result = ''
     var selected_operator_element = $(this);
     var selected_operator = selected_operator_element.text();
+
     expression.push(selected_operator);
+    selected_operator = '';
+    var expression_string = array_to_string(expression);
+    displayResult(expression_string);
+
+    console.log('Expression: ' + expression);
+    console.log('Result: ' + result);
 });
 
 
@@ -58,11 +73,20 @@ function evaluate_expression(expression) {
         result /= parseFloat(expression[i+1])
         return result
       case 'x':
-
         result *= parseFloat(expression[i+1])
         return result
       default:
         result = parseFloat(expression[i])
     }
   }
+}
+
+function array_to_string(array) {
+  var final_string = '';
+  for (var i = 0; i < array.length; i++) {
+    // console.log(array[i]);
+    final_string += array[i];
+    final_string += ' ';
+  }
+  return final_string;
 }
